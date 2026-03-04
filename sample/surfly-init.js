61f3765e323b3c2f404dca7f8be74815bea19203
Ajoy Gonsalves <ajoy@Ajoys-MacBook-Pro-2.local>
2026-03-04 14:51:43 +0100

    feat: test 2

--- sample/surfly-init.js
+++ sample/surfly-init.js
@@ -1,7 +1,21 @@
+(function (s, u, r, f, l, y) {
+    s[f] = s[f] || {
+        init: function () {
+            s[f].q = arguments;
+        },
+    };
+    l = u.createElement(r);
+    y = u.getElementsByTagName(r)[0];
+    l.async = 1;
+    l.src = 'https://surfly.com/surfly.js';
+    y.parentNode.insertBefore(l, y);
+})(window, document, 'script', 'Surfly');
+
 const settings = {
     widget_key: '25444b89443041b1a42abd3be2647567',
     embedded_sessions_only: true,
 };
+
 Surfly.init(settings, function (initResult) {
     if (initResult.success) {
         console.log('All good for happy cobrowsing!');
